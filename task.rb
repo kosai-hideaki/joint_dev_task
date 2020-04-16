32fb6905662c7bd33febfbce1f19ab9d4bb1db87
小齋理恵 <kosairie@MacBook-Air.lan>
2020-04-16 10:59:21 +0900

    質問１〜６に修正。質問７に回答

--- task.rb
+++ task.rb
@@ -5,7 +5,7 @@ def q1
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-  names.push("斎藤")
+  names<<"斎藤"
   puts names
 end
 
@@ -14,7 +14,7 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array1=array1+array2
+  array1 = array1+array2
   puts array1
 end
 
@@ -32,17 +32,16 @@ def q4
   # 以下に回答を記載
 
   # 以下は変更しないで下さい
-  sports=sports.compact
+  sports=sports.compact!
   p sports
 end
 
 def q5
   array1 = []
   array2 = [1, 5, 8, 10]
-　
   # 以下に回答を記載
-  p array1.empty?
-  p array2.empty?
+p array1.empty?
+p array2.empty?
 end
 
 def q6
@@ -57,7 +56,7 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+array.map(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -66,7 +65,9 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+programming_languages.map(&:capitalize)
+upper_case_programming_languages = programming_languages
+upper_case_programming_languages.upcase!
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
